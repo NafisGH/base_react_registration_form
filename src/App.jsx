499d182234b4c644e00eba71679158d3c28634db
Nafis Garifullin <garifullinn@gmail.com>
2024-09-18 09:27:09 +0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Registration_form from './Registration_form'
+
+function App() {
+
+  return (
+    <>
+     <Registration_form/>
+    </>
+  )
+}
+
+export default App
